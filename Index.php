2fd239780d45f0e85d5d595684c15e8fb6e758b0
987486-RinchenWangdi <987486@win.edu.au>
2024-07-29 16:18:13 +1000

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,49 @@
+﻿<?php
+function calculateTriangleArea($base, $height) {
+    return ($base * $height) / 2;
+}
+
+// Example usage:
+$base = 10;
+$height = 5;
+$area = calculateTriangleArea($base, $height);
+echo "The area of the triangle is $area";
+?>
+
+<?php
+function findSingleNumber($arr) {
+    $result = 0;
+    foreach ($arr as $num) {
+        $result ^= $num; // XOR operation
+    }
+    return $result;
+}
+
+$input = array(5, 3, 4, 3, 4);
+$singleNumber = findSingleNumber($input);
+echo "The single number is $singleNumber";
+?>
+
+<?php
+function checkDayType($day) {
+    switch(strtolower($day)) {
+        case 'monday':
+        case 'tuesday':
+        case 'wednesday':
+        case 'thursday':
+        case 'friday':
+            return "It's a weekday.";
+        case 'saturday':
+        case 'sunday':
+            return "It's a weekend.";
+        default:
+            return "Invalid day.";
+    }
+}
+
+// Example usage:
+$day = 'Tuesday';
+$message = checkDayType($day);
+echo $message;
+?>
+
